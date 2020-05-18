ce4ececbb615df796f1f2b6dea681c52aa4722b0
Maxim Yudin <yudin.mn@mail.ru>
2020-05-19 02:27:27 +0300

    first

---
+++ src/components/NoteListItem.vue
@@ -0,0 +1,132 @@
+<template>
+  <div v-show="isShowing" class="note">
+    <note-header
+      :note="note"
+      @click="headerClick"
+      @delete="headerDeleteClick"
+    />
+
+    <!--Предел ограничен 5ю единицами на общей странице заметок -->
+    <todo-list-item
+      v-for="todo in filteredTodos"
+      :key="todo.id"
+      :todo="todo"
+      :isEnabled="!inList"
+    />
+    <!-- Выводим 6е дело с эффектом прозрачности - намекает, что в заметке есть ещё дела -->
+    <button
+      v-show="note.todos.length > LIMIT_FOR_TODOS"
+      class="btn more-todos align-left"
+      @click="buttonAllNoteTasksClick(note)"
+    >
+      Остальные задачи
+    </button>
+  </div>
+</template>
+
+<script>
+import NoteHeader from "../components/NoteHeader.vue";
+import TodoListItem from "../components/TodoListItem.vue";
+
+import { mapActions } from "vuex";
+export default {
+  name: "NoteItem",
+
+  components: {
+    TodoListItem,
+    NoteHeader,
+  },
+
+  props: {
+    inList: {
+      default: false,
+    },
+
+    note: Object,
+    id: String,
+    name: String,
+    todos: Array,
+
+    filter: {
+      default: {
+        did: false,
+        name: "",
+      },
+    },
+  },
+  data() {
+    return {
+      LIMIT_FOR_TODOS: 5,
+      isShowing: true,
+    };
+  },
+
+  watch: {
+    "filteredTodos.length": function(newValue) {
+      this.isShowing = newValue > 0;
+    },
+  },
+
+  computed: {
+    filteredTodos() {
+      const todosdid = [];
+      const todos = [];
+      if (this.filter.did) {
+        todosdid.push(...this.note.todos.filter((todo) => todo.isChecked));
+      } else {
+        todosdid.push(...this.note.todos);
+      }
+
+      if (this.filter.name.length > 0) {
+        todos.push(
+          ...todosdid.filter((todo) =>
+            todo.name.toLowerCase().includes(this.filter.name.toLowerCase())
+          )
+        );
+      } else {
+        todos.push(...todosdid);
+      }
+      return todos.filter((todo, index) => index < this.LIMIT_FOR_TODOS);
+    },
+  },
+
+  methods: {
+    ...mapActions(["RemoveNote"]),
+    headerClick(note) {
+      this.$router.push({ name: "Note", params: { id: note.id } });
+    },
+    headerDeleteClick(note) {
+      this.RemoveNote(note.id);
+    },
+
+    buttonAllNoteTasksClick(note) {
+      this.headerClick(note);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/App.scss";
+
+.dimmer {
+  opacity: 0.5;
+}
+
+.note {
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid $color-blue;
+  margin-bottom: 50px;
+}
+
+.more-todos {
+  @include borderColor($color-pompadour, $color-white, $color-pompadour);
+  width: 30%;
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+.more-todos:hover {
+  @include borderColor($color-white, $color-pompadour, $color-white);
+}
+</style>
---
+++ src/views/NotesList.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="root">
+    <h1>Список заметок</h1>
+    <div class="notes">
+      <div class="row">
+        <div class="align-left">
+          <span>Только выполненные</span>
+          <input v-model="filterDid" type="checkbox" />
+        </div>
+        <div class="align-right">
+          <span>Поиск:</span>
+          <input
+            v-model="filterString"
+            class="name filter"
+            type="text"
+            placeholder="Название задачи"
+          />
+        </div>
+      </div>
+
+      <button class="btn add" @click="buttonNoteAddClick">
+        Добавить карточку
+      </button>
+
+      <note-list-item
+        v-for="note in getNotes"
+        :key="note.id"
+        :note="note"
+        :inList="true"
+        :filter="filter"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import NoteListItem from "../components/NoteListItem.vue";
+
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  components: {
+    NoteListItem,
+  },
+
+  data() {
+    return {
+      filterDid: false,
+      filterString: "",
+    };
+  },
+
+  computed: {
+    ...mapGetters(["notes"]),
+    filter() {
+      return { did: this.filterDid, name: this.filterString };
+    },
+    getNotes() {
+      // если мы ещё не загрузили заметки в стор - делаем это
+      if (!this.notes || this.notes.length === 0) {
+        this.LoadSaveData();
+      }
+
+      return this.notes;
+    },
+  },
+
+  methods: {
+    ...mapActions(["LoadSaveData"]),
+
+    buttonNoteAddClick() {
+      this.$router.push({ name: "Note", params: { id: this.notes.length } });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/App.scss";
+
+.add {
+  @include borderColor($color-blue, $color-white, $color-blue);
+  width: 100%;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  cursor: pointer;
+}
+.add:hover {
+  @include borderColor($color-white, $color-blue, $color-white);
+}
+
+.notes {
+  display: flex;
+  flex-direction: column;
+
+  width: 50%;
+  padding-left: 25%;
+}
+
+.filter {
+  outline: none;
+  border-bottom: 1px solid $color-blue;
+}
+.filter:focus {
+  outline: none;
+  border-bottom: 1px solid $color-red;
+}
+
+
+
+@media screen and (max-width: 900px) {
+  .notes {
+    width: 90%;
+    padding-left: 5%;
+  }
+}
+
+@media handheld {
+  .notes {
+    width: 90%;
+    padding-left: 5%;
+  }
+}
+</style>
